7e23792f93e36012bf62b0cc42ecc36d7fe5b125
AdinMauer <mauer4@gmail.com>
2023-01-21 16:40:56 -0800

    fIRST COMMIT

---
+++ SomeText.py
@@ -0,0 +1,13 @@
+class SomeText:
+    def __init__(self, category, weight):
+        self.category = category
+        self.weight = weight
+
+# Create a SomeText object
+text = SomeText("Example Category", 0.5)
+print(text.category) # Output: Example Category
+print(text.weight) # Output: 0.5
+
+
+
+
