9d370d85270f1a9cc355b0336c9e38641db1617f
eomielan <eomielan@gmail.com>
2023-01-22 00:00:01 -0800

    Update methods & add video

--- src/Video2Text/classify_text.py
+++ src/Video2Text/classify_text.py
@@ -5,7 +5,7 @@ from div_speech import *
 import sys
 import itertools
 
-speech_txt = ""
+global speech_txt
 
 def classify(text: str, verbose=True) -> dict:
     """Classify the input text into categories."""
@@ -56,23 +56,31 @@ def combineWeights(main_topics: dict, section_topics: dict):
             main_topics[topic] = section_topics[topic]*0.5
 
     # sorts dict based on values
-    remaining_topics = {k: v for k, v in sorted(remaining_topics.items(), key=lambda item: item[1], reverse=True)}
+    main_topics = {k: v for k, v in sorted(main_topics.items(), key=lambda item: item[1], reverse=True)}
 
     return dict(itertools.islice(main_topics.items(), 5))
 
 def analyze_text(filename):
     speech = readFile(filename)
-    segments = divide_speech(speech.read())
+    segments = divide_speech(speech)
+    print(len(segments))
     for segment in segments:
         topic_dict = accum_text(segment)
-        print(topic_dict)
         # Call GUI
 
 def accum_text(text):
-    speech_txt += " " + text
-    d1 = classify(text)
-    d2 = classify(speech_txt)
+    global speech_txt
+    d1 = classify(text.strip(), False) if len(text.split(" ")) > 20 else classify(speech_txt.strip(), False)
+    speech_txt = speech_txt + " " + text
+
+    #print("\nd1:", d1, "\n")
+    #print("\n", speech_txt.strip(), "\n")
+
+    d2 = classify(speech_txt, False)
+    #print("\nd2:", d2, "\n")
     x = combineWeights(d1, d2)
+    #print("x:", x)
+    return x
 
 def readFile(file_path: str) -> str:
     """Read from file specified and return file contents as single line string"""
@@ -82,13 +90,15 @@ def readFile(file_path: str) -> str:
     return data
 
 def main():
+    global speech_txt
     args = sys.argv[1:]
-
+    speech_txt = ""
     # scrape video file name from cmd and convert to text
     speech_text_file = Video2Text.vid2text(args[0])
 
     # Analyze speech text file - return sliced dict (size == 5)
     dict = analyze_text(speech_text_file)
+    print(dict)
 
 if __name__ == '__main__':
     main()
